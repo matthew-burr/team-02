299614c8e4370b533b10aa14729ed481529073fc
Matthew Burr <mdburr@outlook.com>
2017-04-22 12:56:33 -0500

    Part I: Create the Expense Interface and Cruise Class

---
+++ src/burr/Destination.java
@@ -0,0 +1,12 @@
+package burr;
+
+/**
+ * Contains three possible destinations
+ *
+ * @author Matthew Burr
+ * @version 1.0
+ * @since 2017.04.22
+ */
+public enum Destination {
+  Mexico, Europe, Japan
+}
